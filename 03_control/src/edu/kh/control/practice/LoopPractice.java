b2dddc66015492fabda20acf7bbef2eea501d9a0
heobae <hbr0901@naver.com>
2025-02-17 01:23:59 +0900

    0216 집에서 보냄

--- 03_control/src/edu/kh/control/practice/LoopPractice.java
+++ 03_control/src/edu/kh/control/practice/LoopPractice.java
@@ -315,5 +315,154 @@ ex.
 		int input = sc.nextInt();
 		
 		
+		for (int row = input; row >= 1; row--) {
+			
+			for (int col = 1; col <= row; col++) {
+				System.out.print("*");
+			}System.out.println();
+		}  
+		
+		
+	}
+	
+	public void practice9() {
+		
+/*
+메소드 명 : public void practice9(){}
+다음과 같은 실행 예제를 구현하세요.
+
+ex.
+정수 입력 : 4
+   *
+  **
+ ***
+****
+
+*/
+		
+		System.out.print("정수 입력: ");
+		int input = sc.nextInt();
+		
+		for(int row = 1; row <= input; row++) {
+			
+			for(int col = 1; col <= input - row; col++) {
+				System.out.print(" ");
+			}
+			for(int col = 1; col <= row; col++) {
+				System.out.print("*");
+			} System.out.println();
+		} 
+	}
+	
+	public void practice10() {
+		
+/*		ex.
+		정수 입력 : 3
+		*
+		**
+		***
+		**
+		*
+*/
+		
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+		
+		for(int row = 1; row <= input; row++) {
+			for(int col = 1; col <= row; col++) {
+				System.out.print("*");
+			} System.out.println();
+		}
+		
+		for(int row = input -1; row >= 1; row--) {
+			for(int col = 1; col <= row; col++) {
+				System.out.print("*");
+			} System.out.println();
+			
+		}
+		
+	
+	}
+	public void practice11() {
+		
+/*
+정수 입력 : 4
+   *
+  ***
+ *****
+*******	
+	
+*/
+		
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+		
+		for(int row = 1; row <= input; row++) {
+			for(int col = 1; col <= input - row; col++) {
+				System.out.print(" ");
+			}  for(int col = 1; col <= row; col++) {
+				System.out.print("*");
+			}  System.out.println();
+		}
+	} //미완성
+
+	public void practice12() {
+		
+/*
+정수 입력 : 5
+*****
+*   *
+*   *
+*   *
+*****	
+	
+*/
+	System.out.print("정수 입력 : ");
+	int input = sc.nextInt();
+	
+	for(int i = 1; i <= input; i++) {
+		System.out.print("*");
+	} System.out.println();
+	
+	for(int row = 1; row <= input - 2; row++) {
+		System.out.print("*");
+	  for(int col = 1; col <= input -2; col++ ) {
+		System.out.print(" ");
+	} System.out.println("*");
+	}
+	for(int i = 1; i <= input; i++) {
+		System.out.print("*");
+	} System.out.println();
+}
+	
+	public void practice13() {
+/*1
+부터 사용자에게 입력 받은 수까지 중에서
+
+1) 2와 3의 배수를 모두 출력하고
+2) 2와 3의 공배수의 개수를 출력하세요.
+
+* ‘공배수’는 둘 이상의 수의 공통인 배수라는 뜻으로 어떤 수를 해당 수들로 나눴을 때
+모두 나머지가 0이 나오는 수
+
+ex.
+자연수 하나를 입력하세요 : 15
+2 3 4 6 8 9 10 12 14 15
+count : 2		
+*/
+		System.out.print("자연수 하나를 입력하세요 :");
+		int input = sc.nextInt();
+		
+		int count = 0;
+		
+		for(int i = 1; i <= input; i++) {
+			if(i % 2 == 0 || i % 3 == 0) {
+				System.out.print(i + " ");
+			} if(i % 2 == 0 && i % 3 == 0) {
+				count++;
+			}
+			
+		} System.out.println();
+		System.out.print("count : " + count);
 	}
 }
--- 03_control/src/edu/kh/control/practice/Run2.java
+++ 03_control/src/edu/kh/control/practice/Run2.java
@@ -11,9 +11,13 @@ public class Run2 {
 		//lp.practice4(); 
 		//lp.practice5();
 		//lp.practice6(); 
-	lp.practice7(); 
-		//lp.practice8()
-		
+	    //lp.practice7(); 
+		//lp.practice8();
+		//lp.practice9();
+		//lp.practice10();
+		//lp.practice11();
+		//lp.practice12();
+		lp.practice13();
 	}
 
 }
